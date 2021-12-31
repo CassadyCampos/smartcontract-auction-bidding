97bce6747e37a5501ba92f0b8e64f38d83d982b6
Cassady <cassady.campos@uleth.ca>
2021-12-30 22:51:43 -0700

    fix amount rounding for gwei

--- src/App.js
+++ src/App.js
@@ -4,25 +4,111 @@ import { ethers } from 'ethers';
 import { parseEther, formatEther } from '@ethersproject/units';
 import Auction from './abis/Auction.json';
 
-const AuctionContractAddress = '0x739D1753632afB77a99EE2827509d28a74EEe26d';
+const AuctionContractAddress = '0xa98D349C336c9b8E29aDd524d67C9672A98622D0';
 const emptyAddress = '0x0000000000000000000000000000000000000000';
 
 
 // display account details
 function App() {
   const [account, setAccount] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [myBid, setMyBid] = useState(0);
+  const [isOwner, setIsOwner] = useState(0);
+  const [highestBid, setHighestBid] = useState(0);
+  const [highestBidder, setHighestBidder] = useState('');
+
+  // 
+  async function initializeProvider() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    return new ethers.Contract(AuctionContractAddress, Auction.abi, signer);
+  }
 
   async function requestAccount() {
     const account = await window.ethereum.request({ method: 'eth_requestAccounts' });
     setAccount(account[0]);
   }
 
-  // 
-  async function intializeProvider() {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    return new ethers.Contract(AuctionContractAddress, Auction.abi, signer);
+  async function fetchMyBid() {
+    if (typeof window.ethereum !== 'undefined') {
+      const contract = await initializeProvider();
+      try {
+        // // const myBid = await contract.bids(account);
+        // console.log("myBid: ", await contract.bids(account));
+        const myBid = 4000000000000000000;
+        // setMyBid(parseFloat(formatEther(myBid.toString())).toPrecision(4));
+        setMyBid(parseFloat(formatEther(myBid.toString())));
+      } catch (e) {
+        console.log('error fetching my bid: ', e);
+      }
+    }
+  }
 
+  async function fetchHighestBid() {
+    if (typeof window.ethereum !== 'undefined') {
+      const contract = await initializeProvider();
+      try {
+        const highestBid = await contract.fetchHighestBid();
+        const { bidAmount, bidder } = highestBid;
+
+        // Convert ammount from Wei to ether and round to 4 decimals
+        setHighestBid(parseFloat(formatEther(bidAmount.toString())).toPrecision(4));
+        setHighestBidder(bidder.toLowerCase());
+      } catch (e) {
+        console.log('error fetching highest bid: ', e);
+      }
+    }
+  }
+
+  async function fetchOwner() {
+    if (typeof window.ethereum !== 'undefined') {
+      const contract = await initializeProvider();
+      try {
+        const owner = await contract.getOwner();
+        setIsOwner(owner.toLowerCase() === account);
+      } catch (e) {
+        console.log('error fetching owner: ', e);
+      }
+    }
+  }
+
+  async function submitBid(event) {
+    event.preventDefault();
+    console.log("here");
+    if (typeof window.ethereum !== 'undefined') {
+      console.log("inside if");
+      const contract = await initializeProvider();
+      try {
+        // user inputs amount in terms of ether, conver tto wei
+        const wei = parseEther(amount);
+        await contract.makeBid({ value: wei });
+
+        // wait for smart contract to emit LogBid event then update component
+        contract.on('LogBid', (_, __) => {
+          fetchMyBid();
+          console.log("inside");
+          fetchHighestBid();
+        });
+        console.log("sent");
+      } catch (e) {
+        console.log('error making bid: ', e);
+      }
+    }
+  }
+
+  async function withdraw() {
+    if (typeof window.ethereum !== 'undefined') {
+      const contract = await initializeProvider();
+      contract.on('LogWithdrawal', (_) => {
+        fetchMyBid();
+        fetchHighestBid();
+      });
+      try {
+        await contract.withdraw();
+      } catch (e) {
+        console.log('error withdrawing fund: ', e);
+      }
+    }
   }
 
   // request account
@@ -30,11 +116,44 @@ function App() {
     requestAccount();
   }, []);
 
+  useEffect(() => {
+    if (account) {
+      fetchOwner();
+      fetchMyBid();
+      fetchHighestBid();
+    }
+  }, [account]);
+
   return (
     <div>
       <div>
-        <div>Connected Account: {account}</div>
+        <div>Connected Account: {account} ETH</div>
+        <div>My Bid: {myBid} ETH</div>
+        <div>Auction Highest Bid Amount: {highestBid}</div>
+        <div>
+          Auction Highest Bidder: {' '}
+          {highestBidder === emptyAddress
+            ? 'null'
+            : highestBidder === account
+              ? 'Me'
+              : highestBidder}
+        </div>
       </div>
+
+      {!isOwner ? (
+        <form onSubmit={submitBid}>
+          <input
+            value={amount}
+            onChange={(event) => setAmount(event.target.value)}
+            name="Bid Amount"
+            type="number"
+            placeholder="Enter Bid Amount"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
