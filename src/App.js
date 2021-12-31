2d7a4732aa4f8a1591a9496ae4c0d65a8d97ad6f
Cassady <cassady.campos@uleth.ca>
2021-12-30 20:28:35 -0700

    connectedd to metamask

--- src/App.js
+++ src/App.js
@@ -4,24 +4,38 @@ import { ethers } from 'ethers';
 import { parseEther, formatEther } from '@ethersproject/units';
 import Auction from './abis/Auction.json';
 
+const AuctionContractAddress = '0x739D1753632afB77a99EE2827509d28a74EEe26d';
 const emptyAddress = '0x0000000000000000000000000000000000000000';
+
 
 // display account details
 function App() {
   const [account, setAccount] = useState('');
 
-  async function requestAcount() {
+  async function requestAccount() {
     const account = await window.ethereum.request({ method: 'eth_requestAccounts' });
     setAccount(account[0]);
   }
-  
+
+  // 
+  async function intializeProvider() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    return new ethers.Contract(AuctionContractAddress, Auction.abi, signer);
+
+  }
+
+  // request account
+  useEffect(() => {
+    requestAccount();
+  }, []);
 
   return (
-   <div>
-     <div>
-       <div>Connected Account: {account}</div>
-     </div>
-   </div>
+    <div>
+      <div>
+        <div>Connected Account: {account}</div>
+      </div>
+    </div>
   );
 }
 
